f906c77f1670c32329622ed71d503ae1327d2d03
geoff langenderfer <geoff.langenderfer>
2021-01-07 16:33:46 -0500

    working on elm frontend

--- login_page/src/Main.elm
+++ login_page/src/Main.elm
@@ -1,30 +1,75 @@
 module Main exposing (..)
-type Model
-  = Loading 
---what is this for?  | LoadedOne User
-  | LoadingProblem Problem
-  | Loaded LoadedModel
 
-type Problem
-  = ServerProblem Http.Error
-  | DatabaseProblem String
-   
-type alias LoadedModel = 
-  { user: User
-  , problem: Maybe Problem
-  , integrations: List Integration
+-- Input a user name and password. Make sure the password matches.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/forms.html
+--
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { email : String
+  , password : String
   }
 
-viewLoaded: LoadedModel -> Html Msg
 
-view: Model -> Html Msg
-view model = 
-  case model of 
-    Loading ->
-      viewLoadingSpinner
-    LoadedOne loadedUser ->
-    LoadingProblem loadProblem ->
-    Loaded loadedModel ->
-      viewLoaded loadedModel
-      
-      
+init : Model
+init =
+  Model "" "" 
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Email String
+  | Password String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Email email ->
+      { model | email = email }
+
+    Password password ->
+      { model | password = password }
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  
+  div []
+    [ h1 [] [text "Reporting: Automated"]
+    , viewInput "email" "Username" model.email Email
+    , viewInput "password" "Password" model.password Password
+    , button [] [ text "Login" ]
+    ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
